3d3ab199a9978e87d3bbf656fb57a37b4efb92ad
Michael Kabatek <mkabatek@gmail.com>
2018-10-11 00:43:22 -0600

    Updated for instagram 10/10/2018

--- gramcracker.js
+++ gramcracker.js
@@ -1,88 +1,190 @@
-var followersBoxClass = document.getElementsByClassName("_fd86t");
-var followingBoxClass = document.getElementsByClassName("_fd86t");
+//Instagram classes
+var followBoxString           = "g47SY"
+var unfollowModalButtonString = "aOOlW -Cab_   "
+var followButtonString        = "oF4XW sqdOP  L3NKy      "
+var unFollowButtonString      = "oF4XW sqdOP  L3NKy   _8A5w5   "
+var scrollBoxString           = "isgrP"
+
+var followersBoxClass = document.getElementsByClassName(followBoxString)[1];
+var followingBoxClass = document.getElementsByClassName(followBoxString)[2];
+var unFollowModal = document.getElementsByClassName(unfollowModalButtonString)[0];
+
+//Constants
+var TENTH_SECOND_DELAY = 100
+var ONE_SECOND_DELAY   = 1000
+var THREE_SECOND_DELAY = 3000
 
 //Get int for number of people to unfollow calculation
-var myFollowerInt;
+var myInitialFollowers;
+var itr;
 
 //Global setting parameters
-var homeName = "<your_instagram_username>";
-var homeUrl = "https://instagram.com/<your_instagram_username>";
+var rootUrl = "https://instagram.com/" 
+var homeName = "noisapp";
+var homeUrl = rootUrl + homeName;
 var globalIndex;
 
+//looping parameters 
+var NumberItr     = 2;              //Total number of people you want to follow/unfollow
+var BaseFollowers = 500;            //Base number of followers you want to keep
+var tMax          = 120000;         //follow/unfollow time max
+var tMin          = 60000;          //follow/unfollow time min
+var delta         = (tMax - tMin);  //Time interval between follow/unfollow
+var stMax         = 500;            //Scroll time max
+var stMin         = 100;            //Scroll time min
+var T             = 0;              //Total elapsed time
 
 //Accounts you want to follow
 var masterUrlList = [
 
-
-"https://www.instagram.com/usatf/",
-"https://www.instagram.com/nikerunning/",
-"https://www.instagram.com/robinnyc/",
-"https://www.instagram.com/usainbolt/",
-"https://www.instagram.com/instarunners/",
-"https://www.instagram.com/finnrunner/",
-"https://www.instagram.com/karagoucher/",
-"https://www.instagram.com/runnerscommunity/",
-"https://www.instagram.com/hungryrunnergirl/",
-"https://www.instagram.com/thrashermag/",
-"https://www.instagram.com/transworldskate/",
-"https://www.instagram.com/nycrunningmama/",
-"https://www.instagram.com/JeredGruber/",
-"https://www.instagram.com/TaylorPhinney/",
-"https://www.instagram.com/theRadavist/",
-"https://www.instagram.com/tiffanycromwell/",
-"https://www.instagram.com/PreferredMode/",
-"https://www.instagram.com/vansgirls/",
-"https://www.instagram.com/vansskate/",
-"https://www.instagram.com/LachlanMorton/",
-"https://www.instagram.com/TeamSky/",
-"https://www.instagram.com/volcomskate/",
-"https://www.instagram.com/berrics/",
-"https://www.instagram.com/redbull/",
-"https://www.instagram.com/nikesb/",
-"https://www.instagram.com/KatieHolden/",
-"https://www.instagram.com/manualforspeed/",
-"https://www.instagram.com/angelocalilap/",
-"https://www.instagram.com/fireflybicycles/",
-"https://www.instagram.com/nolifelikethislife/",
-"https://www.instagram.com/mtn2mtn/",
-"https://www.instagram.com/woodwardcopper/",
-"https://www.instagram.com/mashsf/",
-"https://www.instagram.com/allyshale/",
-"https://www.instagram.com/garrett_chow/",
-"https://www.instagram.com/sweetrideusa/",
-"https://www.instagram.com/strangenotes/",
-"https://www.instagram.com/skullcandy/",
-"https://www.instagram.com/gopro/",
-"https://www.instagram.com/freepeople/",
-"https://www.instagram.com/outdoortech/",
-"https://www.instagram.com/downhilladiction/",
-"https://www.instagram.com/bronze56k/"
+"highcascade",
+"perisher_parks",
+"rockstarenergy",
+"electric_snow",
+"lifeproof",
+"alek_oestreng",
+"hannahteter",
+"cardronanz",
+"jamieanderson",
+"thrashermag",
+"transworldskate",
+"twsnow",
+"newschoolers",
+"christyprior",
+"snowboardermag",
+"toohardsquad",
+"bonesbearings",
+"vansgirls",
+"vansskate",
+"vanssnow",
+"chrisrussell_mbk",
+"volcomskate",
+"berrics",
+"redbull",
+"nikesb",
+"monsterenergy",
+"volklskis",
+"burtonsnowboards",
+"burtongirls",
+"atomicski",
+"keystonea51",
+"woodwardcopper",
+"windellscamp",
+"allyshale",
+"lindseyvonn",
+"jacksonhole",
+"steepskiing",
+"strangenotes",
+"skullcandy",
+"gopro",
+"gnusnowboards",
+"romesnowboards",
+"lynseydyer",
+"skiingmagazine",
+"freeskiermagazine",
+"jameswoodsy",
+"freepeople",
+"outdoortech",
+"downhilladiction",
+"bronze56k",
+"snowparklaax"
 
 ];
-//looping parameters 
-var NumberItr = 400;
-var BaseFollowers = 770;
-var tMax = 120000; //follow/unfollow time max milliseconds
-var tMin = 60000; //follow/unfollow time min milliseconds
 
-var stMax = 2000; //scroll time max milliseconds
-var stMin = 1000; //scroll time min milliseconds
+//follow/Unfollow function
+var followUnfollow = function(elem, itr, boxType){
+    scroll(elem, itr, boxType);
+}
 
+var followerLoop = function(currentElements, numberOfFollowIter, i) {
+    //Follow/Unfollow code
+    var dt = Math.random() * delta + tMin;
+    T = T + dt;                 //  total time elapsed
+    setTimeout(function () {
+        console.log('delta t: ' + dt);
+        console.log('Total time elapsed (ms): ' + T);
+        console.log('Current item: ' + i);
+        console.log('Final item: ' + (currentElements.length - numberOfFollowIter));          
+        currentElements[i-1].click();
+        setTimeout(function(){
+            var unfollowModalButton = document.getElementsByClassName(unfollowModalButtonString)[0]
+            if (unfollowModalButton == undefined) {
+                //Windows not showing move on
+                checkLoopingStatus(currentElements, numberOfFollowIter, i)
+            } else {
+                //Window is showing click the modal
+                unfollowModalButton.click()
+                checkLoopingStatus(currentElements, numberOfFollowIter, i)
+            }
+        },ONE_SECOND_DELAY)
+    }, dt)
+}
 
-if(localStorage.getItem('indexObject') == null || localStorage.getItem('indexObject') == "null" || localStorage.getItem('indexObject') == "undefined"){
+var checkLoopingStatus = function(currentElements, numberOfFollowIter, i) {
+    i--;                     //  increment the counter
+    if (i > (currentElements.length - numberOfFollowIter)) {            
+        followerLoop(currentElements, numberOfFollowIter, i);             //  ..  again which will trigger another 
+    }
+    else{
+        console.log("finished looping!! goto next url");
+        if(window.location.href.indexOf(homeName) > -1) {
+            window.setTimeout(function(){ window.location.replace(rootUrl + masterUrlList[globalIndex.index]);},5000);
+        } else {
+            window.setTimeout(function(){ window.location.replace(homeUrl);},5000);
+        }   
+    }
+}
+
+var collectionToArray = function(collection, boxType){
+    elements = Array.from(collection)
+    elements = elements.filter(obj => {
+        return obj.className === boxType
+    }) 
+    return elements
+}
+
+var scroll = function(currentElements, iterations, boxType) {
+    var x = document.getElementsByClassName(scrollBoxString);
+	var timeMax  = stMax;
+	var timeMin   = stMin; 
+	var delta = (timeMax - timeMin); 
+	var dt = Math.random() * delta + timeMin;
+	var T = 0;
+					 
+	console.log('delta t: ' + dt);
+    console.log('T: ' + T);
+    T = T + dt;           //  total time
+    dt = Math.random() * delta + timeMin; //set dt based on random number and timeSpan
+    
+    setTimeout(function () {    //  call a 3s setTimeout when the loop is called
+        console.log('delta t: ' + dt);
+        console.log('T: ' + T);
+        console.log('Elements in list: ' + currentElements.length);	  
+        x[0].scrollTop += 10000;
+        
+        if (currentElements.length < iterations - 1) { 
+            currentElements = document.getElementsByClassName(boxType);      
+            scroll(collectionToArray(currentElements, boxType), iterations, boxType);             //  ..  again which will trigger another 
+        } else {
+            console.log('############## scroll finished ################')
+            followerLoop(currentElements, currentElements.length, iterations); 
+        }                      
+    }, dt)
+}
+
+if(localStorage.getItem('indexObject') == null || 
+    localStorage.getItem('indexObject') == "null" || 
+    localStorage.getItem('indexObject') == "undefined"){
 	
-	// if indexObject not yet defined initialize
-	// Put the object into storage
-	var indexObject = { 'index': 0};
+	//initialize the index object if not yet defined
+	var indexObject = {'index': 0};
 	localStorage.setItem('indexObject', JSON.stringify(indexObject));
 	globalIndex = indexObject;
 }
 else{
-
-	//Get the index object
+	//Get the index object for the next account to follow
 	var indexObject = localStorage.getItem('indexObject');
 	globalIndex = JSON.parse(indexObject);
-	
 }
 
 console.log('Global index:');
@@ -92,252 +194,43 @@ console.log(globalIndex);
 var interval = setInterval(function() {
 	if(document.readyState === 'complete') {
 		clearInterval(interval);
-		
 		console.log('Good to go');
-
-		myFollowerInt = Number(followingBoxClass[2].innerHTML.replace(',',''));
-
-		//check if we are at home, if so unfollow, if not follow
-
+        myInitialFollowers = Number(followingBoxClass.innerHTML.replace(',',''));
+		//check if we are at home, if so unfollow accounts
 		if(window.location.href.indexOf(homeName) > -1) {
-			
+            //At this point our personal instagram page has been loaded 
+            //We will now unfollow accounts
 			globalIndex.index++;
 			localStorage.setItem('indexObject', JSON.stringify(globalIndex));
-
-			//load your own page and unfollow
-			followersBoxClass[2].click();
-			unfollow();
-			//u();
-			
+            followingBoxClass.click();
+            setTimeout(function(){
+                var itr
+                var possibleIterations = Number(myInitialFollowers) - Number(BaseFollowers)
+                if (NumberItr > possibleIterations) {
+                    itr = possibleIterations
+                } else {
+                    itr = NumberItr
+                }
+                var elem = document.getElementsByClassName(unFollowButtonString);
+                console.log('BaseFollowers: ' + BaseFollowers);
+                console.log('CurrentFollower: ' + myInitialFollowers);
+                console.log('Total iterations possible: ' + possibleIterations);
+                console.log('Total iterations entered: ' + NumberItr);
+            
+                followUnfollow(collectionToArray(elem), itr, unFollowButtonString);
+            }, THREE_SECOND_DELAY);
 		}
 		else{
-
-
 			localStorage.setItem('indexObject', JSON.stringify(globalIndex));
-			//load other's page and follow
-			followersBoxClass[1].click();
-			follow();
-			//f();
-			
+			//At this point our target instagram page has been loaded 
+            //We will now follow it's accounts
+            followersBoxClass.click();
+            setTimeout(function(){
+                var itr = NumberItr
+                var elem = document.getElementsByClassName(followButtonString);
+                console.log('Total iterations: ' + itr);
+                followUnfollow(collectionToArray(elem), itr, followButtonString);
+            }, THREE_SECOND_DELAY)
 		}
-		
-		
-		
 	}    
-}, 100);
-
-
-
-
-
-//test functions
-function u(){
-	window.setTimeout(function(){ window.location.replace(masterUrlList[globalIndex.index]);},5000);
-}
-
-function f(){
-	window.setTimeout(function(){ window.location.replace(homeUrl);},5000);
-}
-
-
-//follow function
-function follow(){
-
-	var buttonClassString = "_qv64e _gexxb _4tgw8 _njrw0";
-	var scrollClassString = "_gs38e";
-
-	var elem = document.getElementsByClassName(buttonClassString);
-	var x = document.getElementsByClassName(scrollClassString);                    //  set your counter to number of elements
-	var itr = NumberItr;
-	var timeMax  = stMax;
-	var timeMin   = stMin; 
-	var timeSpan = (timeMax - timeMin) + timeMin;
-	var delta = (timeMax - timeMin); 
-	var dt = Math.random() * delta + timeMin;
-	var T = 0;
-					 
-	console.log('delta t: ' + dt);
-	console.log('T: ' + T);
-
-
-	function scroll () {
-	   T = T + dt;           //  total time
-	   dt = Math.random() * delta + timeMin; //set dt based on random number and timeSpan
-	   
-	   setTimeout(function () {    //  call a 3s setTimeout when the loop is called
-		  console.log('delta t: ' + dt);
-		  console.log('T: ' + T);
-		  console.log('Elements: ' + elem.length + '/' + itr);	  
-		  x[0].scrollTop += 10000;
-		  
-		  if (elem.length < itr - 1) {            
-			 scroll();             //  ..  again which will trigger another 
-		  }                        //  ..  setTimeout()
-	   }, dt)
-	}
-
-	scroll();
-
-
-
-	function checkFlag() {
-		var elem = document.getElementsByClassName(buttonClassString);
-		if(elem.length < itr) {
-			window.setTimeout(checkFlag, 500); /* this checks the flag every 100 milliseconds*/
-			console.log('... scrolling ...')	
-		} else {
-		  
-			console.log('############## scroll finished ################')
-			//Follow code
-
-			
-			var elem = document.getElementsByClassName(buttonClassString);
-			var itrF =  NumberItr;                      // number of elements to follow/unfollow
-			var i = NumberItr;                     //  set your counter to number of elements
-			var total = i;
-			var timeMax  = tMax;
-			var timeMin   = tMin; 
-			var timeSpan = (timeMax - timeMin) + timeMin;
-			var delta = (timeMax - timeMin); 
-			var dt = Math.random() * delta + timeMin;
-			var T = 0;
-							 
-			console.log('delta t: ' + dt);
-			console.log('T: ' + T);
-			console.log('Current item: ' + i);
-			console.log('Final item: ' + (total - itrF));
-			console.log('Number of entires: ' + elem.length);
-
-			function myLoop () {
-			   T = T + dt;           //  total time
-			   dt = Math.random() * delta + timeMin; //set dt based on random number and timeSpan
-			   
-			   setTimeout(function () {    //  call a 3s setTimeout when the loop is called
-				  console.log('delta t: ' + dt);
-				  console.log('T: ' + T);
-				  console.log('Current item: ' + i);
-				  console.log('Final item: ' + (total - itrF));          
-				  elem[i].click();
-				  i--;                     //  increment the counter
-				  if (i >= (total - itrF)) {            
-					 myLoop();             //  ..  again which will trigger another 
-				  }
-				  else{
-					console.log("finished looping!! go back home") 
-					window.setTimeout(function(){window.location.replace(homeUrl);},5000);
-					  
-					  
-				  }
-			   }, dt)
-			}
-
-			myLoop(); 
-		  
-		  
-		}
-	}
-	checkFlag();
-
-
-
-}
-
-//unfollow function
-function unfollow (){
-
-	var buttonClassString = "_qv64e _t78yp _4tgw8 _njrw0";
-	var scrollClassString = "_gs38e";
-
-	var elem = document.getElementsByClassName(buttonClassString);
-	var x = document.getElementsByClassName(scrollClassString);                    //  set your counter to number of elements
-	var itr = myFollowerInt - BaseFollowers;
-	var timeMax  = stMax;
-	var timeMin   = stMin; 
-	var timeSpan = (timeMax - timeMin) + timeMin;
-	var delta = (timeMax - timeMin); 
-	var dt = Math.random() * delta + timeMin;
-	var T = 0;
-					 
-	console.log('delta t: ' + dt);
-	console.log('T: ' + T);
-
-
-	function scroll () {
-	   T = T + dt;           //  total time
-	   dt = Math.random() * delta + timeMin; //set dt based on random number and timeSpan
-	   
-	   setTimeout(function () {    //  call a 3s setTimeout when the loop is called
-		  console.log('delta t: ' + dt);
-		  console.log('T: ' + T);
-		  console.log('Elements: ' + elem.length + '/' + itr);	  
-		  x[0].scrollTop += 10000;
-		  
-		  if (elem.length < itr - 1) {            
-			 scroll();             //  ..  again which will trigger another 
-		  }                        //  ..  setTimeout()
-	   }, dt)
-	}
-
-	scroll();
-
-
-
-	function checkFlag() {
-		var elem = document.getElementsByClassName(buttonClassString);
-		if(elem.length < itr) {
-			window.setTimeout(checkFlag, 500); /* this checks the flag every 100 milliseconds*/
-			console.log('... scrolling ...')	
-		} else {
-		  
-			console.log('############## scroll finished ################')
-			//Follow code
-
-			
-			var elem = document.getElementsByClassName(buttonClassString);
-			var itrF =  myFollowerInt - BaseFollowers;                      // number of elements to follow/unfollow
-			var i = itrF;                     //  set your counter to number of elements
-			var total = i;
-			var timeMax  = tMax;
-			var timeMin   = tMin; 
-			var timeSpan = (timeMax - timeMin) + timeMin;
-			var delta = (timeMax - timeMin); 
-			var dt = Math.random() * delta + timeMin;
-			var T = 0;
-							 
-			console.log('delta t: ' + dt);
-			console.log('T: ' + T);
-			console.log('Current item: ' + i);
-			console.log('Final item: ' + (total - itrF));
-			console.log('Number of entires: ' + elem.length);
-
-			function myLoop () {
-			   T = T + dt;           //  total time
-			   dt = Math.random() * delta + timeMin; //set dt based on random number and timeSpan
-			   
-			   setTimeout(function () {    //  call a 3s setTimeout when the loop is called
-				  console.log('delta t: ' + dt);
-				  console.log('T: ' + T);
-				  console.log('Current item: ' + i);
-				  console.log('Final item: ' + (total - itrF));          
-				  elem[i].click();
-				  i--;                     //  increment the counter
-				  if (i >= (total - itrF)) {            
-					 myLoop();             //  ..  again which will trigger another 
-				  }
-				  else{
-					console.log("finished looping!! goto next");
-					window.setTimeout(function(){ window.location.replace(masterUrlList[globalIndex.index]);},5000);
-										 
-					  
-				  }
-			   }, dt)
-			}
-
-			myLoop(); 
-		  
-		  
-		}
-	}
-	checkFlag();
-
-}
+}, TENTH_SECOND_DELAY);
